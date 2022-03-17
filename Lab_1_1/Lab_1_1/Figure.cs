01a426c6b7ddc8df4453aaa7d33bd88645cbb3e3
IgorKow <92447174+IgorKow@users.noreply.github.com>
2022-03-17 20:35:38 +0300

    Add files via upload

---
+++ Lab_1_1/Lab_1_1/Figure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab_1_1
+{
+   abstract public class Figure
+   {
+        abstract public void PrintFigure(Pen pen, Graphics graphic);
+
+   }
+
+}
---
+++ Lab_1_1/Lab_1_1/Hexagon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab_1_1
+{
+    class Hexagon: Figure
+    {
+        int x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6;
+
+        public Hexagon(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+            this.x4 = x4;
+            this.y4 = y4;
+            this.x5 = x5;
+            this.y5 = y5;
+            this.x6 = x6;
+            this.y6 = y6;
+        }
+
+        public override void PrintFigure(Pen pen, Graphics graphic)
+        {
+            Point a = new Point(x1, y1);
+            Point b = new Point(x2, y2);
+            Point c = new Point(x3, y3);
+            Point d = new Point(x4, y4);
+            Point e = new Point(x5, y5);
+            Point f = new Point(x6, y6);
+
+            graphic.DrawLine(pen, a, b);
+            graphic.DrawLine(pen, b, c);
+            graphic.DrawLine(pen, c, d);
+            graphic.DrawLine(pen, d, e);
+            graphic.DrawLine(pen, e, f);
+            graphic.DrawLine(pen, f, a);
+        }
+    }
+}
